535ae4d7815cd94a23e4e3816d7f628b7c0cd071
Austin <ataraxia@Austins-MacBook-Air.local>
2023-07-14 12:47:45 -0300

    Refactored router to use browserRoutes variable new version 6.14.

--- resourceful/src/App.tsx
+++ resourceful/src/App.tsx
@@ -1,19 +1,35 @@
 import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route, 
+  Outlet
+   } from "react-router-dom"
+
 import Header from './components/Header/Header';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import LogInPage from './pages/LogInPage/LogInPage';
+import Layout from './pages/Layout/Layout';
+import Homepage from './pages/Homepage/Homepage';
+
 
 function App() {
 
+  const browserRoutes = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Homepage />}></Route>
+      <Route path='/sign-up' element={<SignUpPage />}></Route>
+      <Route path='/log-in' element={<LogInPage />}></Route>
+    </Route>
+
+
+  ))
+
   return (
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/log-in" element={<LogInPage/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <RouterProvider router={browserRoutes} />
+    </>
   )
 }
 
---
+++ resourceful/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <>
+            <h1>HOME</h1>
+        </>
+    );
+};
+
+export default Homepage;
